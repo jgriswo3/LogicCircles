1786a291136a2aa3052b4013cc984888f8364d7d
jonathanGriswoldCPCC <jgriswo3@email.cpcc.edu>
2021-01-26 00:36:51 -0500

    updated scenes and scripts

--- Assets/Scripts/DirectionalNode.cs
+++ Assets/Scripts/DirectionalNode.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionalNode : MonoBehaviour
 {
+    SpriteRenderer sr;
     public Sprite none;
     public Sprite left;
     public Sprite right;
     public string current;
-    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        sr.sprite = left;
         SwitchSprite();
     }
 
@@ -22,19 +23,21 @@ public class DirectionalNode : MonoBehaviour
     {
         
     }
-
     void SwitchSprite()
     {
         if(current == "left")
         {
+            Debug.Log("Choose Left");
             sr.sprite = left;
         }
         else if(current == "right")
         {
+            Debug.Log("Choose Right");
             sr.sprite = right;
         }
         else
         {
+            Debug.Log("Choose None");
             sr.sprite = none;
         }
     }
